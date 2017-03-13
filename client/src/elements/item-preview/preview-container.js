de5ec73b068de0770313b10d3c8c8328d1cb5945
Beni <beni.buess@nzz.ch>
2017-03-13 18:51:29 +0100

    remove previously loaded styles and scripts in item preview

--- client/src/elements/item-preview/preview-container.js
+++ client/src/elements/item-preview/preview-container.js
@@ -8,6 +8,8 @@ export class PreviewContainer {
   @bindable width
   @bindable renderingInfo
 
+  insertedElements = [];
+
   constructor(element) {
     this.element = element;
   }
@@ -20,84 +22,93 @@ export class PreviewContainer {
     this.showPreview(this.renderingInfo);
   }
 
-  showPreview(renderingInfo) {
+  async showPreview(renderingInfo) {
     if (!this.previewElement) {
       return;
     }
+    
     if (!renderingInfo) {
       this.previewElement.innerHTML = '';
       return;
     }
 
-    return qEnv.QServerBaseUrl
-      .then(QServerBaseUrl => {
-    
-        // load the stylesheets
-        if (Array.isArray(renderingInfo.stylesheets)) {
-          renderingInfo.stylesheets
-            .map(stylesheet => {
-              if (!stylesheet.url && stylesheet.path) {
-                stylesheet.url = `${QServerBaseUrl}${stylesheet.path}`
-              }
-              return stylesheet
-            })
-            .map(stylesheet => {
-              if (stylesheet.url) {
-                let link = document.createElement('link');
-                link.type = 'text/css';
-                link.rel = "stylesheet";
-                link.href = stylesheet.url;
-                this.element.shadowRoot.insertBefore(link, this.element.shadowRoot.firstChild);
-              } else if (stylesheet.content) {
-                let style = document.createElement('style');
-                style.type = 'text/css';
-                style.appendChild(document.createTextNode(stylesheet.content));
-                this.element.shadowRoot.insertBefore(style, this.element.shadowRoot.firstChild);
-              }
-            })
-        }
+    const QServerBaseUrl = await qEnv.QServerBaseUrl;
 
-        // load the scripts one after the other
-        if (Array.isArray(renderingInfo.scripts)) {
-          renderingInfo.scripts = renderingInfo.scripts
-            .filter(script => script.name !== 'system.js') // do not laod system.js if the tool wants it, it's already here
-            .map(script => {
-              if (script.path) {
-                script.url = `${QServerBaseUrl}${script.path}`;
-              }
-              return script;
-            })
-
-          loadAllScripts(renderingInfo.scripts, this.element.shadowRoot);
-        }
-
-        this.previewElement.innerHTML = renderingInfo.markup;
-      })
-  }
-}
-
-function loadAllScripts(scripts, parentElement, index = 0, callback) {
-  if (scripts && scripts[index] && scripts[index].url) {
-    let script = scripts[index];
-    let scriptElement = document.createElement('script');
-
-    if (script.url) {
-      scriptElement.src = script.url;
-      script.async = true;
-
-      scriptElement.onload = () => {
-        console.log('script loaded')
-        loadAllScripts(scripts, parentElement, index + 1, callback)
-      }
-      parentElement.appendChild(scriptElement);
-
-    } else if (script.content) {
-      scriptElement.innerHTML = script.content;
-      parentElement.appendChild(scriptElement);
-      loadAllScripts(scripts, parentElement, index + 1, callback)
+    // remove all previously inserted elements
+    while(this.insertedElements.length > 0) {
+      let element = this.insertedElements.pop();
+      element.parentNode.removeChild(element);
     }
 
-  } else if (typeof callback === 'function') {
-    callback();
+    // load the stylesheets
+    if (Array.isArray(renderingInfo.stylesheets)) {
+      renderingInfo.stylesheets
+        .map(stylesheet => {
+          if (!stylesheet.url && stylesheet.path) {
+            stylesheet.url = `${QServerBaseUrl}${stylesheet.path}`
+          }
+          return stylesheet
+        })
+        .map(stylesheet => {
+          if (stylesheet.url) {
+            let link = document.createElement('link');
+            link.type = 'text/css';
+            link.rel = "stylesheet";
+            link.href = stylesheet.url;
+            this.insertedElements.push(link);
+            this.element.shadowRoot.appendChild(link);
+          } else if (stylesheet.content) {
+            let style = document.createElement('style');
+            style.type = 'text/css';
+            style.appendChild(document.createTextNode(stylesheet.content));
+            this.insertedElements.push(style);
+            this.element.shadowRoot.appendChild(style);
+          }
+        })
+    }
+
+    // load the scripts one after the other
+    if (Array.isArray(renderingInfo.scripts)) {
+      renderingInfo.scripts = renderingInfo.scripts
+        .filter(script => script.name !== 'system.js') // do not laod system.js if the tool wants it, it's already here
+        .map(script => {
+          if (script.path) {
+            script.url = `${QServerBaseUrl}${script.path}`;
+          }
+          return script;
+        })
+
+      this.loadAllScripts(renderingInfo.scripts, this.previewElement);
+    }
+
+    if (renderingInfo.markup) {
+      this.previewElement.innerHTML = renderingInfo.markup;
+    }
+  }
+
+  loadAllScripts(scripts, index = 0, callback) {
+    if (scripts && scripts[index] && scripts[index].url) {
+      let script = scripts[index];
+      let scriptElement = document.createElement('script');
+
+      if (script.url) {
+        scriptElement.src = script.url;
+        script.async = true;
+
+        scriptElement.onload = () => {
+          loadAllScripts(scripts, index + 1, callback)
+        }
+        this.insertedElements.push(scriptElement);
+        this.element.shadowRoot.appendChild(scriptElement);
+      } else if (script.content) {
+        scriptElement.innerHTML = script.content;
+        this.insertedElements.push(scriptElement);
+        this.element.shadowRoot.appendChild(scriptElement);
+        loadAllScripts(scripts, index + 1, callback)
+      }
+
+    } else if (typeof callback === 'function') {
+      callback();
+    }
   }
 }
