9f84a6d87f3694107208419aa2684504545deb76
Beni <beni.buess@nzz.ch>
2017-03-27 14:36:58 +0200

    improves code doc and error handling in schema-editor-geojson-point

--- client/src/elements/schema-editor/schema-editor-geojson-point.js
+++ client/src/elements/schema-editor/schema-editor-geojson-point.js
@@ -1,4 +1,6 @@
-import { bindable, inject, Loader } from 'aurelia-framework';
+import { bindable, inject, Loader, LogManager } from 'aurelia-framework';
+
+const log = LogManager.getLogger('Q');
 
 import qEnv from 'resources/qEnv';
 import QConfig from 'resources/QConfig';
@@ -26,18 +28,33 @@ export class SchemaEditorGeojsonPoint {
   }
 
   async attached() {
+    this.showLoadingError = false;
+
     // if we already have a map, return here
     // this happens when array entry order is changed.
     if (this.map) {
       return;
     }
 
+    // if window.Leaflet is not defined, we load it async here using the aurelia loader
+    // as we need Leaflet later, we need to await the loading
     if (!window.Leaflet) {
-      window.Leaflet = window.L = await this.loader.loadModule('leaflet');
-      this.loader.loadModule('npm:leaflet@1.0.3/dist/leaflet.css!');
+      try {
+        window.Leaflet = window.L = await this.loader.loadModule('leaflet');
+        this.loader.loadModule('npm:leaflet@1.0.3/dist/leaflet.css!');
+      } catch (e) {
+        log.error(e);
+      }
+    }
+    if (!window.Leaflet) {
+      log.error('window.Leaflet is not defined after loading leaflet');
+      this.showLoadingError = true;
+      return;
     }
 
-    if (!Leaflet.control.geocoder) {
+    // if we do not have a Leaflet.control.geocoder yet, we load the module async using the aurelia loader
+    // this failed in some tests because of weird module format. But it worked always, so we ignore any loading error here...
+    if (!window.Leaflet.control.geocoder) {
       try {
         this.loader.loadModule('npm:leaflet-geocoder-mapzen@1.8.0/dist/leaflet-geocoder-mapzen.css!');
         await this.loader.loadModule('leaflet-geocoder-mapzen');
@@ -45,47 +62,60 @@ export class SchemaEditorGeojsonPoint {
         // leaflet-geocoder-mapzen is probably loaded, nevermind the error here
       }
     }
+    // ... test again if it's here
+    if (!window.Leaflet.control.geocoder) {
+      log.error('window.Leaflet.control.geocoder is not defined after loading leaflet-geocoder-mapzen');
+      this.showLoadingError = true;
+      return;
+    }
 
-    this.markerPinIcon = Leaflet.divIcon({
+    // define our own marker icon for the pin
+    this.markerPinIcon = window.Leaflet.divIcon({
       className: 'q-map-editor-pin',
       html: iconPinSvg,
       iconSize: [52, 52],
       iconAnchor: [26, 38]
     });
 
-    this.map = Leaflet.map(this.mapContainer, {
+    // initialise the map and set the view to NZZ building
+    this.map = window.Leaflet.map(this.mapContainer, {
       zoomControl: false, // we add this later after the search control
       touchZoom: 'center',
       scrollWheelZoom: 'center'
     }).setView([47.36521171867246, 8.547435700893404], 13);
 
+    // we need a mapzen key for the geocoder, this has to be defined in ENV
+    // if it's not here, we do not add a geocoder
     const mapzenApiKey = await qEnv.mapzenApiKey;
-    this.geocoder = Leaflet.control.geocoder(mapzenApiKey, {
-      attribution: null,
-      fullWidth: true,
-      markers: false,
-      focus: false
-    });
+    if (mapzenApiKey === undefined) {
+      log.info('no mapzenApiKey defined, will not load geocoder for geojson-point editor');
+    } else {
+      this.geocoder = window.Leaflet.control.geocoder(mapzenApiKey, {
+        attribution: null,
+        fullWidth: true,
+        markers: false,
+        focus: false
+      });
 
-    this.zoomControl = Leaflet.control.zoom({
+      this.geocoder.on('select', e => {
+        this.geocoder.collapse();
+        this.data.geometry = e.feature.geometry;
+        this.data.properties.label = e.feature.properties.name;
+        if (this.change) {
+          this.change();
+        }
+        this.updateMarker();
+      });
+    }
+
+    this.zoomControl = window.Leaflet.control.zoom({
       position: 'topleft'
-    });
-
-    this.geocoder.on('select', e => {
-      this.geocoder.collapse();
-      this.data.geometry = e.feature.geometry;
-      this.data.properties.label = e.feature.properties.name;
-      if (this.change) {
-        this.change();
-      }
-      this.updateMarker();
     });
 
     // allows to add a pin by clicking the map only if there is no pin yet
     this.map.on('click', e => {
       if (!this.pin) {
-        this.data.geometry.coordinates[1] = e.latlng.lat;
-        this.data.geometry.coordinates[0] = e.latlng.lng;
+        this.data.geometry.coordinates = [e.latlng.lng, e.latlng.lat];
         this.updateMarker();
         if (this.change) {
           this.change();
@@ -99,17 +129,21 @@ export class SchemaEditorGeojsonPoint {
     if (this.map) {
       this.resolveMapInit();
 
-      this.geocoder.addTo(this.map);
-      this.geocoder.expand();
-      if (this.geocoder._input) {
-        this.geocoder._input.focus();
+      // only add the geocoder if we have one
+      if (this.geocoder) {
+        this.geocoder.addTo(this.map);
+        this.geocoder.expand();
+        if (this.geocoder._input) {
+          this.geocoder._input.focus();
+        }
       }
 
       this.zoomControl.addTo(this.map);
-      Leaflet.tileLayer(layerConfig.url, layerConfig.config).addTo(this.map);
+      window.Leaflet.tileLayer(layerConfig.url, layerConfig.config).addTo(this.map);
     }
   }
 
+  // set the pin to the map if we already have Point data
   async updateMarker(setMapView = false) {
     if (isNaN(this.data.geometry.coordinates[0]) || isNaN(this.data.geometry.coordinates[1])) {
       if (this.pin) {
@@ -119,6 +153,7 @@ export class SchemaEditorGeojsonPoint {
       return;
     }
 
+    // if there already is a pin, update it's location
     if (this.pin) {
       if (this.pin && this.pin.setLatLng) {
         this.pin.setLatLng(this.data.geometry.coordinates.slice().reverse());
@@ -127,7 +162,7 @@ export class SchemaEditorGeojsonPoint {
         }
       }
     } else {
-      this.pin = Leaflet.marker(this.data.geometry.coordinates.slice().reverse(), {
+      this.pin = window.Leaflet.marker(this.data.geometry.coordinates.slice().reverse(), {
         icon: this.markerPinIcon,
         clickable: false,
         keyboard: false,
