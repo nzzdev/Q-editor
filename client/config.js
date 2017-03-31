37bca768487fb1e4a9b68761c2fbdcab3b170151
Sharon Funke <sharon.funke@nzz.ch>
2017-03-31 13:47:00 +0200

    Add color picker input field support

--- client/config.js
+++ client/config.js
@@ -241,8 +241,7 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "ieee754": "npm:ieee754@1.1.8",
       "isarray": "npm:isarray@1.0.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.7": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
