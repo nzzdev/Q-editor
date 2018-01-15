0dc43eab802bed70565947707d68b358b42f687e
benib <beni.buess@nzz.ch>
2018-01-15 14:24:54 +0100

    (schema-editor) input placeholder configurabel via Q:options

--- client/src/elements/schema-editor/schema-editor-string.js
+++ client/src/elements/schema-editor/schema-editor-string.js
@@ -1,12 +1,22 @@
-import { bindable } from 'aurelia-framework';
-import { checkAvailability } from 'resources/schemaEditorDecorators.js';
+import { bindable } from "aurelia-framework";
+import { checkAvailability } from "resources/schemaEditorDecorators.js";
 
 @checkAvailability()
 export class SchemaEditorString {
-
   @bindable schema;
   @bindable data;
   @bindable change;
   @bindable required;
 
+  options = {};
+
+  schemaChanged() {
+    this.applyOptions();
+  }
+
+  applyOptions() {
+    if (this.schema.hasOwnProperty("Q:options")) {
+      this.options = Object.assign(this.options, this.schema["Q:options"]);
+    }
+  }
 }
