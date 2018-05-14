33d8626937a46557e534611e4d992bf44ab03aa2
Manuel Roth <manuel.roth@nzz.ch>
2018-05-14 12:26:30 +0200

    Move notifications to schema-editor-wrapper level

--- client/src/elements/schema-editor/schema-editor-wrapper.js
+++ client/src/elements/schema-editor/schema-editor-wrapper.js
@@ -1,6 +1,8 @@
-import { bindable } from "aurelia-framework";
+import { bindable, inject } from "aurelia-framework";
 import { getType } from "./helpers";
+import { Validation } from "resources/Validation.js";
 
+@inject(Validation)
 export class SchemaEditorWrapper {
   @bindable schema;
   @bindable data;
@@ -8,7 +10,24 @@ export class SchemaEditorWrapper {
   @bindable required;
   @bindable noObjectTitle;
 
-  constructor() {
+  notification = {};
+  options = {};
+
+  constructor(validation) {
+    this.validation = validation;
     this.getType = getType;
+  }
+
+  async validate(element, type) {
+    console.log("Event type: " + type);
+    if (this.schema.hasOwnProperty("Q:options")) {
+      this.options = Object.assign(this.options, this.schema["Q:options"]);
+    }
+    this.notification = await this.validation.validate(
+      element,
+      this.options.validationRules,
+      this.schema,
+      this.data
+    );
   }
 }
