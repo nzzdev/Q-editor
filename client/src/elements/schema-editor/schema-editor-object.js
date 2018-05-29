52f2c07cd05db6ae9b39063edd3ff397bad03b9b
benib <beni.buess@nzz.ch>
2018-05-29 17:42:59 +0400

    (schema-editor) set data for schema objects to empty object if data is undefined.

--- client/src/elements/schema-editor/schema-editor-object.js
+++ client/src/elements/schema-editor/schema-editor-object.js
@@ -14,6 +14,16 @@ export class SchemaEditorObject {
     this.isRequired = isRequired;
   }
 
+  // this makes it possible to add new objects to the schema without running a migration
+  // existing items are not updated with new defaults from the schema, thus not setting this to an empty object
+  // results in errors when the schema-editor tries to access a property on undefined
+  // maybe this would be better solved by updating existing items with the current schema defaults
+  dataChanged() {
+    if (this.data === undefined) {
+      this.data = {};
+    }
+  }
+
   isCompact(schema) {
     if (schema && schema["Q:options"] && schema["Q:options"].compact) {
       return true;
