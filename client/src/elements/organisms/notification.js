9c3be8fcd3d78aa4e7dd0f7081d2b7ebd664489c
Manuel Roth <manuel.roth@nzz.ch>
2018-05-30 18:00:42 +0200

    Add icons to medium and high priority notifications

--- client/src/elements/organisms/notification.js
+++ client/src/elements/organisms/notification.js
@@ -3,7 +3,15 @@ import { bindable } from "aurelia-framework";
 export class Notification {
   @bindable notification;
 
+  hasIcon = false;
+  iconName = "";
+
   constructor(notification) {
     this.notification = notification;
+  }
+
+  notificationChanged() {
+    this.hasIcon = ["medium", "high"].includes(this.notification.priority);
+    this.iconName = `notification-${this.notification.priority}`;
   }
 }
