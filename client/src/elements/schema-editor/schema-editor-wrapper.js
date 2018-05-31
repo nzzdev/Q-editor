4453a6924500dc4d701455ad361da74ef660ea6a
Manuel Roth <manuel.roth@nzz.ch>
2018-05-31 11:45:05 +0200

    Use input event as trigger for validation

--- client/src/elements/schema-editor/schema-editor-wrapper.js
+++ client/src/elements/schema-editor/schema-editor-wrapper.js
@@ -28,7 +28,6 @@ export class SchemaEditorWrapper {
   }
 
   async validate(event) {
-    event.stopPropagation();
     this.notification = await this.validation.validate(
       this.options.validationRules,
       event.target
