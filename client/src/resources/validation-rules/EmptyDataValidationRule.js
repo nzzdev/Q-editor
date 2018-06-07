dffdadcb0bcef273bd87efa9b1d66a0da84862bd
Manuel Roth <manuel.roth@nzz.ch>
2018-06-07 08:52:34 +0200

    Empty data notification should have low priority

--- client/src/resources/validation-rules/EmptyDataValidationRule.js
+++ client/src/resources/validation-rules/EmptyDataValidationRule.js
@@ -2,7 +2,7 @@ export default class EmptyDataValidationRule {
   validate(data) {
     return {
       showNotification: data.length === 1 && data[0].length === 0,
-      priority: "medium"
+      priority: "low"
     };
   }
 }
