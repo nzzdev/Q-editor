42b6031a88e4189032adc6e1ae9bf42bc016d5d0
Manuel Roth <manuel.roth@nzz.ch>
2018-06-12 11:07:52 +0200

    Add validationConfig env var to /env route

--- routes/env.js
+++ routes/env.js
@@ -2,7 +2,8 @@ const env = {
   QServerBaseUrl: process.env.Q_SERVER_BASE_URL,
   loginMessage: process.env.LOGIN_MESSAGE || null,
   devLogging: process.env.DEV_LOGGING || false,
-  pushState: process.env.PUSH_STATE || true
+  pushState: process.env.PUSH_STATE || true,
+  validationConfig: JSON.parse(process.env.VALIDATION_LIMITS) || {}
 };
 
 if (process.env.PLAYGROUND) {
