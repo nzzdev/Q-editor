f669ac04271211f7a22a903f2058a75f57e04397
Manuel Roth <manuel.roth@nzz.ch>
2018-06-20 17:24:58 +0200

    Implement use as blueprint option

--- client/src/elements/molecules/item-list-entry.js
+++ client/src/elements/molecules/item-list-entry.js
@@ -1,17 +1,21 @@
 import { bindable, inject } from "aurelia-framework";
 import { Router } from "aurelia-router";
+import { Notification } from "aurelia-notification";
+import { I18N } from "aurelia-i18n";
 import User from "resources/User";
 import ToolsInfo from "resources/ToolsInfo";
 
-@inject(Element, ToolsInfo, User, Router)
+@inject(Element, ToolsInfo, User, Router, Notification, I18N)
 export class ItemListEntry {
   @bindable item;
 
-  constructor(element, toolsInfo, user, router) {
+  constructor(element, toolsInfo, user, router, notification, i18n) {
     this.element = element;
     this.toolsInfo = toolsInfo;
     this.user = user;
     this.router = router;
+    this.notification = notification;
+    this.i18n = i18n;
   }
 
   itemChanged() {
@@ -24,6 +28,12 @@ export class ItemListEntry {
         this.iconSvg = itemTool.icon;
       }
     });
+  }
+
+  toggleOptionDropdown() {
+    this.optionsDropdown.classList.toggle(
+      "item-list-entry__options-dropdown--show"
+    );
   }
 
   activateItem() {
@@ -40,9 +50,14 @@ export class ItemListEntry {
     });
   }
 
-  toggleOptionDropdown() {
-    this.optionsDropdown.classList.toggle(
-      "item-list-entry__options-dropdown--show"
-    );
+  async blueprint() {
+    try {
+      await this.item.blueprint();
+      this.item.conf.title = this.i18n.tr("item.blueprintTitlePrefix");
+      let tool = this.item.conf.tool.replace(new RegExp("-", "g"), "_");
+      this.router.navigate(`/editor/${tool}/${this.item.conf._id}`);
+    } catch (e) {
+      this.notification.error("notification.failedToLoadItem");
+    }
   }
 }
