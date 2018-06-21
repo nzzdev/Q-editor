014eda28fbbf0d01f5754246ed2cd3cc2ed6202b
benib <beni.buess@nzz.ch>
2018-06-21 20:59:25 +0400

    remove delete-button/confirm-button/button-yes/button-no and it's color settings

--- client/src/elements/molecules/index.js
+++ client/src/elements/molecules/index.js
@@ -1,7 +1,5 @@
 export function configure(frameworkConfiguration) {
   frameworkConfiguration
-    .globalResources("./confirm-button")
-    .globalResources("./delete-button.html")
     .globalResources("./embed-code")
     .globalResources("./item-list-entry")
     .globalResources("./dropdown-menu")
