fe1e20c0e3259608e9f5ffd19d17cd48c21bc785
benib <beni.buess@nzz.ch>
2018-07-03 09:33:52 +0200

    (editor) disabled the save/activate buttons if the item is currently saving. Never save an item while it is saving

--- client/src/elements/atoms/icon-button.js
+++ client/src/elements/atoms/icon-button.js
@@ -1,10 +1,16 @@
 import { bindable } from "aurelia-framework";
 
 export class IconButton {
+  @bindable click;
   @bindable icon;
   @bindable code;
   @bindable size;
   @bindable iconSize;
   @bindable tabindex;
+  @bindable disabled;
   @bindable type = "button";
+
+  handleClick(event) {
+    debugger;
+  }
 }
