05d5aa6807cbc084c1e3b0f9d3d8df0e3230a59f
benib <beni.buess@nzz.ch>
2018-10-02 12:21:49 +0200

    (schema-editor) correctly handle Q:default for array items

--- client/src/resources/ObjectFromSchemaGenerator.js
+++ client/src/resources/ObjectFromSchemaGenerator.js
@@ -39,11 +39,9 @@ export default class ObjectFromSchemaGenerator {
           }
         }
       }
-      if (schema.default !== undefined) {
-        const defaultValue = this.getDefaultOrNull(schema);
-        if (defaultValue) {
-          array = defaultValue;
-        }
+      const defaultValue = this.getDefaultOrNull(schema);
+      if (defaultValue !== null) {
+        array = defaultValue;
       }
       return array;
     } else if (schema.type === "object") {
