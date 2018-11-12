46247ff4cade89b1e8ee839109b17e41bdbc208b
Philip Küng <kueng.philip@gmail.com>
2018-11-12 14:04:11 +0100

    added list of tasks

--- client/src/pages/tasks.js
+++ client/src/pages/tasks.js
@@ -3,5 +3,19 @@ import { inject } from "aurelia-framework";
 @inject()
 export default class Tasks {
   constructor() {}
-  async activate() {}
+  tasks = [];
+  renderingInfoTask = {
+    name: "Rendering Info",
+    loadTask: function() {
+      console.log("up and running");
+    }
+  };
+
+  async activate() {
+    this.tasks.push(this.renderingInfoTask);
+  }
+
+  executeTask(task) {
+    task.loadTask();
+  }
 }
