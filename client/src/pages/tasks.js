c5e3c3c44e8ee711231e902ae1bb3c83c65659be
Philip Küng <kueng.philip@gmail.com>
2018-11-15 10:22:28 +0100

    load task

--- client/src/pages/tasks.js
+++ client/src/pages/tasks.js
@@ -1,21 +1,57 @@
 import { inject } from "aurelia-framework";
+import qEnv from "resources/qEnv.js";
 
 @inject()
 export default class Tasks {
   constructor() {}
   tasks = [];
-  renderingInfoTask = {
-    name: "Rendering Info",
-    loadTask: function() {
-      console.log("up and running");
-    }
+  selectedTask = {};
+  output = [];
+  taskInput = {
+    selectedItemId: ""
   };
+  renderingInfoTask = {};
 
   async activate() {
+    // get all tasks
+    this.renderingInfoTask = {
+      name: "Rendering Info",
+      endpoint: "tasks/renderingInfo",
+      schema: {
+        input: {
+          $schema: "http://json-schema.org/draft-04/schema#",
+          type: "string"
+        },
+        output: {}
+      }
+    };
     this.tasks.push(this.renderingInfoTask);
   }
 
-  executeTask(task) {
-    task.loadTask();
+  loadTask(selectedTask) {
+    this.selectedTask = selectedTask;
+  }
+
+  executeTask() {
+    return qEnv.QServerBaseUrl.then(QServerBaseUrl => {
+      return fetch(
+        `${QServerBaseUrl}/${this.selectedTask.endpoint}/${
+          this.taskInput.selectedItemId
+        }`
+      );
+    })
+      .then(res => {
+        if (res.ok && res.status >= 200 && res.status < 400) {
+          return res.json();
+        }
+        throw res;
+      })
+      .then(renderingInfo => {
+        this.output = renderingInfo;
+      });
+  }
+
+  handleChange() {
+    console.log("test");
   }
 }
