81514b407aa6ba20db22a5d73ec3a14722b2ad7d
benib <beni.buess@nzz.ch>
2019-06-22 12:14:25 +0200

    (radio-button-group) fix matching of checked radio buttons by binding on model instead of value (to make sure types stay the same)

--- client/src/elements/molecules/radio-button-group.js
+++ client/src/elements/molecules/radio-button-group.js
@@ -8,11 +8,4 @@ export class RadioButtonGroup {
   constructor() {
     this.name = `radio-button-group-${Math.floor(Math.random() * 10000)}`;
   }
-
-  valueMatcher(a, b) {
-    // disable the linter as we do not want strict type checking here because number/string
-    /* eslint-disable */
-    return a == b;
-    /* eslint-enable */
-  }
 }
