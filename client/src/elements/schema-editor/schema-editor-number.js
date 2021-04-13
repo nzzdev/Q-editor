0190665c2159286e339770a6c549063bdf4bd261
Nicolas Staub <nicolas.staub@nzz.ch>
2021-04-13 13:32:45 +0200

    add logging for test

--- client/src/elements/schema-editor/schema-editor-number.js
+++ client/src/elements/schema-editor/schema-editor-number.js
@@ -30,8 +30,10 @@ export class SchemaEditorNumber {
   }
 
   onWheel(event) {
+    console.log("wheel!")
     if (event && event.target === document.activeElement) {
       // Bluring the input field prevents changing the value while using the mouse wheel
+      console.log("focus!")
       event.target.blur();
     }
     return true;
